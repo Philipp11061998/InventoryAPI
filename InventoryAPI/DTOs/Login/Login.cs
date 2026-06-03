a4ec413a2cfd14b70d087f7bb724509e947a0225
Philipp11061998 <philippkraut25@gmail.com>
2026-06-03 16:43:31 +0200

    Secrets aus appsettings entfernt (auch wenn trotzdem hardcodet, richtige Stelle)

--- InventoryAPI/DTOs/Login/Login.cs
+++ InventoryAPI/DTOs/Login/Login.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace InventoryAPI.DTOs
 {
     public class Login
     {
+        [Required, MaxLength(50)]
         public string Username { get; set; } = string.Empty;
+        [Required]
         public string Password { get; set; } = string.Empty;
     }
 }
--- InventoryAPI/DTOs/Login/Register.cs
+++ InventoryAPI/DTOs/Login/Register.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace InventoryAPI.DTOs
 {
     public class Register
     {
+        [Required, MaxLength(50)]
         public string Username { get; set; } = string.Empty;
+        [Required]
         public string Password { get; set; } = string.Empty;
     }
 }
